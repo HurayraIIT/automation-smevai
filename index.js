bd910e7c7ad35983991356574cdeeb38c25c251b
Abu Hurayra <hurayraiit@gmail.com>
2022-04-20 10:49:20 +0600

    Refactored index.js

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
+/**
+ * Bismillahir Rahmanir Rahim
+ * This is the entry point for automation.
+ */
+
 "use strict";
 const {Builder, By, Key, until} = require('selenium-webdriver');
 const config = require('./util/config.json');
+const LoginPage = require('./Pages/login_page');
 
-async function example() {
-    const driver = await new Builder().forBrowser('firefox').build();
-    await driver.get(config.live_site);
-    await driver.findElement(By.name('username')).sendKeys(config.email, Key.RETURN);
-    await driver.findElement(By.name('password')).sendKeys(config.password, Key.RETURN);
+function run() {
+    
+    const driver = new Builder().forBrowser('firefox').build();
+    let loginPage = new LoginPage(driver);
+    loginPage.login();
     
 }
 
-example();
+run();
