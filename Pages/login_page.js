0166cea26fd65e2ac913549e0c6ce8b47505898c
Abu Hurayra <hurayraiit@gmail.com>
2022-04-21 10:18:12 +0600

    Added Register Page Base Code

--- Pages/login_page.js
+++ Pages/login_page.js
@@ -4,18 +4,17 @@
  */
 
 "use strict";
-const {Builder, By, Key, until} = require('selenium-webdriver');
+const { Builder, By, Key, until } = require('selenium-webdriver');
 const config = require('../util/config.json');
 
 class LoginPage {
     constructor(driver) {
         this.driver = driver;
     }
-    
+
     async login() {
-        
-        await this.driver.get(config.live_site);
-        await this.driver.findElement(By.name('username')).sendKeys(config.email, Key.RETURN);
+        await this.driver.get(config.site_to_test);
+        await this.driver.findElement(By.name('username')).sendKeys(config.trading_admin_email, Key.RETURN);
         await this.driver.findElement(By.name('password')).sendKeys(config.password, Key.RETURN);
     }
 }
--- index.js
+++ index.js
@@ -4,16 +4,17 @@
  */
 
 "use strict";
+
 const {Builder, By, Key, until} = require('selenium-webdriver');
 const config = require('./util/config.json');
 const LoginPage = require('./Pages/login_page');
+const RegisterPage = require('./Pages/register_page');
 
 function run() {
     
     const driver = new Builder().forBrowser('firefox').build();
     let loginPage = new LoginPage(driver);
     loginPage.login();
-    
 }
 
 run();
