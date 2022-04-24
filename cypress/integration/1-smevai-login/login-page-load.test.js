7691fcfa3d65886b239d07f0ccd0b88ed15f338b
Abu Hurayra <hurayraiit@gmail.com>
2022-04-24 10:46:15 +0600

    login page test refactor

--- cypress/integration/1-smevai-login/login-page-load.test.js
+++ cypress/integration/1-smevai-login/login-page-load.test.js
@@ -5,31 +5,29 @@
  * registration page and forgot password 
  * page loads properly or not.
  */
-describe('Login Page Load Test', () => {
-    it('Login Page', () => {
+describe('Login Page Test', () => {
+    beforeEach(() => {
         cy.visit('https://app.smevai.com')
+    })
+    
+    it('Test the main login page', () => {
         cy.contains('Phone Number / Email')
         cy.url().should('include', 'smevai.com/login')
-        
+    })
+    
+    it('Test the signup/register page', () => {
         // Go to the signup page and verify
         cy.contains('Signup').click()
         cy.url().should('include', 'smevai.com/register')
         cy.contains('Register')
         cy.contains('First Name')
         cy.contains('Already have an account?')
-        
-        // Go back to the login page
-        cy.go('back')
-        cy.url().should('include', 'smevai.com/login')
-        
+    })
+    
+    it('Test the forgot password page', () => {
         // Go to the forgot password page
         cy.contains('Forgot Password?').click()
         cy.url().should('include', 'smevai.com/password/reset')
         cy.contains('Forgot')
-        
-        // Go back to the login page
-        cy.go('back')
-        cy.url().should('include', 'smevai.com/login')
-        
     })
 })
