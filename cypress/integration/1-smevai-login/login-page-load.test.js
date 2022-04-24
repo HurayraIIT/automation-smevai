bb4f57f24216cf1636f636023f897776fbc33188
Abu Hurayra <hurayraiit@gmail.com>
2022-04-24 11:15:52 +0600

    Refactor Login page test

--- cypress/integration/1-smevai-login/login-page-load.test.js
+++ cypress/integration/1-smevai-login/login-page-load.test.js
@@ -5,29 +5,37 @@
  * registration page and forgot password 
  * page loads properly or not.
  */
+
+const config = require('../../../util/config.json');
+
 describe('Login Page Test', () => {
     beforeEach(() => {
-        cy.visit('https://app.smevai.com')
-    })
+        cy.restoreLocalStorage();
+        cy.visit(config.live_site);
+    });
+    
+    afterEach(() => {
+        cy.saveLocalStorage();
+    });
     
     it('Test the main login page', () => {
-        cy.contains('Phone Number / Email')
-        cy.url().should('include', 'smevai.com/login')
-    })
+        cy.contains('Phone Number / Email');
+        cy.url().should('include', 'smevai.com/login');
+    });
     
     it('Test the signup/register page', () => {
         // Go to the signup page and verify
-        cy.contains('Signup').click()
-        cy.url().should('include', 'smevai.com/register')
-        cy.contains('Register')
-        cy.contains('First Name')
-        cy.contains('Already have an account?')
-    })
+        cy.contains('Signup').click();
+        cy.url().should('include', 'smevai.com/register');
+        cy.contains('Register');
+        cy.contains('First Name');
+        cy.contains('Already have an account?');
+    });
     
     it('Test the forgot password page', () => {
         // Go to the forgot password page
-        cy.contains('Forgot Password?').click()
-        cy.url().should('include', 'smevai.com/password/reset')
-        cy.contains('Forgot')
-    })
-})
+        cy.contains('Forgot Password?').click();
+        cy.url().should('include', 'smevai.com/password/reset');
+        cy.contains('Forgot');
+    });
+});
