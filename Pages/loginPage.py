4686c86a9e5fce057374fda7df2df4b297d77776
Abu Hurayra <hurayraiit@gmail.com>
2022-07-25 12:40:27 +0600

    login test

--- Pages/loginPage.py
+++ Pages/loginPage.py
@@ -2,9 +2,11 @@ class LoginPage:
     def __init__(self, driver):
         self.driver = driver
 
-        self.username_textbox_name = f""
-        self.password_textbox_name = f""
-        self.submit_button_name = f""
+        self.login_page_heading_text_xpath = f"/html/body/div[1]/div[1]/div[2]/h2"
+        self.login_page_heading_text = f"Login"
+        self.username_textbox_name = f"username"
+        self.password_textbox_name = f"password"
+        self.submit_button_xpath = f"//button[@class='button button__themeColor button__block']"
 
         def enter_username(self, username):
             pass
--- Tests/loginTest.py
+++ Tests/loginTest.py
@@ -3,6 +3,8 @@ import sys
 import os
 import unittest
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from Pages.loginPage import LoginPage
 from Pages.dashboardPage import DashboardPage
 
@@ -17,7 +19,14 @@ class LoginTest(unittest.TestCase):
     def test_login_valid(self):
         driver = self.driver
         driver.get("https://app.smevai.com")
-        time.sleep(2)
+
+        login = LoginPage(driver)
+        driver.find_element(By.NAME, login.username_textbox_name).send_keys("wpdabh+autota1@gmail.com")
+        driver.find_element(By.NAME, login.password_textbox_name).send_keys("pass1234")
+        driver.find_element(By.XPATH, login.submit_button_xpath).click()
+
+
+        time.sleep(7)
 
     @classmethod
     def tearDownClass(cls) -> None:
